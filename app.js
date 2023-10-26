f1dac8112b966e25e1eefd5014fbb8f931030671
BelimiiYA <belim7vlad@gmail.com>
2023-10-27 00:30:45 +0200

    auth

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
+import passport from "passport";
 
-const contactsRouter = require('./routes/api/contacts')
+import "./config/config-passport.js";
 
-const app = express()
+import contactsRouter from "./routes/api/contacts.js";
+import usersRouter from "./routes/api/users.js";
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const app = express();
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(passport.initialize());
 
-app.use('/api/contacts', contactsRouter)
+app.use("/api/contacts", contactsRouter);
+app.use("/users", usersRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
-
+  res.status(404).json({ message: "Not found" });
+});
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
-module.exports = app
+export default app;
