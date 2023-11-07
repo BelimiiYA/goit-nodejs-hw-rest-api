88c2bf895ad1eba68c8d41776030ee87ac6b4615
BelimiiYA <belim7vlad@gmail.com>
2023-11-07 22:02:27 +0100

    fix

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
-import express from 'express';
-import logger from 'morgan';
-import cors from 'cors';
-import passport from 'passport';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
+import "dotenv/config";
 
-import './config/config-passport.js';
+import contactsRouter from "./routes/api/contacts-router.js";
+import authRouter from "./routes/api/auth-router.js";
 
-import contactsRouter from './routes/api/contacts.js';
-import usersRouter from './routes/api/users.js';
 const app = express();
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(passport.initialize());
+app.use(express.static("public"));
 
-app.use(express.static('public'));
-
-app.use('/api/contacts', contactsRouter);
-app.use('/users', usersRouter);
+app.use("/api/users", authRouter);
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-	res.status(404).json({ message: 'Not found' });
+  res.status(404).json({ message: "Not found" });
 });
+
 app.use((err, req, res, next) => {
-	res.status(500).json({ message: err.message });
+  const { status = 500, message = "server error" } = err;
+  res.status(status).json({ message });
 });
 
 export default app;
---
+++ models/hooks.js
@@ -0,0 +1,10 @@
+export const handleSaveError = (err, data, next) => {
+  const { name, code } = err;
+  err.status = name === "MongoServerError" && code === 11000 ? 409 : 400;
+  next();
+};
+
+export const runUpdateValidation = function (next) {
+  this.options.runValidators = true;
+  next();
+};
